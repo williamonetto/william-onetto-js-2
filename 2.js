6ff4a59517d570136b6ea323471389b60effd346
William Onetto <willonet.biz@gmail.com>
2024-05-06 16:45:37 +0200

    william onetto js2

---
+++ 2.js
@@ -0,0 +1,22 @@
+let a = 1 
+let b = 2
+let c = 3
+let d = 4
+let e = 5 
+
+console.log('La somma tra i numeri equivale a' + (a + b + c + d + e) + 'e la media equivale a' + ((a + b + c + d + e) / 5))
+
+let anno = 2024
+let nascita = 2004
+
+console.log(nascita)
+console.log(100 - (anno - nascita))
+console.log('Hai 20 anni e ti mancano 80 anni per compierne 100')
+
+let totali = 44
+let gatti_file = 6
+let file = totali / gatti_file
+
+console.log(Math.floor(file))
+console.log('Ci sono' + (Math.floor(file)) + 'file di gatti e ne mancano 4 per una nuova fila, con un avanzo di' + (totali % gatti_file))
+
